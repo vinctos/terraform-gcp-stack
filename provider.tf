012884e95c7a4893b3eea6db11d6ae75472e686c
Vinayak S <vinayaks439@gmail.com>
2021-09-12 00:45:23 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,41 @@
+provider "google" {
+  project = var.project_id
+}
+
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 0.10.4"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.62.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 3.64.0"
+    }
+    gsuite = {
+      source  = "deviavir/gsuite"
+      version = "~> 0.1.61"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0.3"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+
+  backend "remote" {
+    organization = var.tfe_organisation_name
+
+    workspaces {
+      name = var.workspacename
+    }
+  }
+}
