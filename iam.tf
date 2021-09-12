8a8d6852d206ea8f02b0f705eb0b1d24dc1044cc
Vinayak S <vinayaks439@gmail.com>
2021-09-12 12:58:34 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -3,3 +3,14 @@ resource "google_service_account" "terraform_sa" {
   display_name = "Service Account for terraform cloud in ${var.environment}"
   project = data.google_project.self.project_id
 }
+
+data "google_service_account" "terraform-env" {
+  account_id = "terraform-${var.environment}"
+  project = data.google_project.self.project_id
+}
+
+resource "google_service_account_iam_member" "owner-account-iam" {
+  service_account_id = data.google_service_account.terraform-env.account_id
+  role               = "roles/owner"
+  member             = "serviceAccount:${data.google_service_account.terraform-env.email}"
+}
