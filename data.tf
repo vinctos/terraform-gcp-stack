2093aba0181b5c122c60fde6ca5f73b187da3d1c
Vinayak S <vinayaks439@gmail.com>
2021-09-14 21:10:55 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -3,3 +3,7 @@ data "google_project" "self" {
 
 data "google_client_config" "current" {
 }
+
+data "google_service_account" "object_viewer" {
+  account_id = "projects/vc-fallen-${var.environment}/serviceaccounts/terraform-${var.environment}@vc-fallen-${var.environment}.iam.gserviceaccount.com"
+}
