23ef91b95bd39ff0c4aad1ab28818ceb910622df
Vinayak S <vinayaks439@gmail.com>
2021-09-14 21:19:37 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -8,6 +8,6 @@ data "google_service_account" "vc-fallen-sa" {
   account_id = "projects/vc-fallen-${var.environment}/serviceaccounts/terraform-${var.environment}@vc-fallen-${var.environment}.iam.gserviceaccount.com"
 }
 
-data "google_compute_address" "test-addr" {
+data "google_compute_address" "test_addr" {
   name = "test-disk"
 }
