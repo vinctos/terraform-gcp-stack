7d2f427ac8924fd4e806f55f4e2376995816071a
Tejas <47742145+tejasjayaram@users.noreply.github.com>
2021-09-16 22:53:35 +0530

    Create cluster.tf
    adding cluster resource

---
+++ cluster.tf
@@ -0,0 +1,29 @@
+
+resource "google_container_cluster" "google_container_cluster" {
+  name     = "my-gke-cluster"
+  location = "us-central1"
+  remove_default_node_pool = true
+  initial_node_count       = 1
+  count = var.environment == "devint" ? 1: 0
+}
+
+resource "google_container_node_pool" "devint_preemptible_nodes" {
+  name       = "test-node-pool"
+  location   = "n1-standard-1"
+  cluster    = google_container_cluster.devintcluster.name
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+
+      service_account {
+    email  = data.google_service_account.vc-fallen-sa.email
+    scopes = ["cloud-platform"]
+  }
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+  depends_on = [google_container_cluster.google_container_cluster]
+}
