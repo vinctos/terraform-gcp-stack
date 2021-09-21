1948e6faf65a63099c8c00dae56d02bff4c46572
Vinayak S <vinayaks439@gmail.com>
2021-09-21 22:37:16 +0530

    Update instance_image.tf

--- instance_image.tf
+++ instance_image.tf
@@ -3,5 +3,5 @@ resource "google_compute_instance_from_machine_image" "tpl" {
   name     = "instance-from-machine-image"
   zone     = "us-central1-a"
 
-  source_machine_image = "https://www.googleapis.com/compute/beta/projects/vc-fallen-${var.environment}/global/machineImages/sad-image"
+  source_machine_image = "https://www.googleapis.com/compute/beta/projects/vc-fallen-${var.environment}/global/images/sad-image"
 }
