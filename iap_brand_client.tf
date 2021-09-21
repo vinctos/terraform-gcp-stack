2eef0537e6bf857e962da1afff73b40765cdbb66
Vinayak S <vinayaks439@gmail.com>
2021-09-21 21:57:18 +0530

    Create iap_brand_client.tf

---
+++ iap_brand_client.tf
@@ -0,0 +1,10 @@
+resource "google_project_service" "project_service" {
+  project = data.google_project.self.project_id
+  service = "iap.googleapis.com"
+}
+
+resource "google_iap_brand" "project_brand" {
+  support_email     = "support@example.com"
+  application_title = "Cloud IAP protected Application"
+  project           = google_project_service.project_service.project
+}
