3b15debed4eddc293f350dbbb0ea339db5408195
Vinayak S <vinayaks439@gmail.com>
2021-09-21 22:14:56 +0530

    Update iap_brand_client.tf

--- iap_brand_client.tf
+++ iap_brand_client.tf
@@ -4,7 +4,7 @@ resource "google_project_service" "project_service" {
 }
 
 resource "google_iap_brand" "project_brand" {
-  support_email     = "vinayak439@gmail.com"
+  support_email     = "anonasaptech@gmail.com"
   application_title = "Cloud IAP protected Application"
   project           = data.google_project.self.project_id
   depends_on = [google_project_service.project_service]
