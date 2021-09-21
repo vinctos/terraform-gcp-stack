ba73885f3e577cc88bef540e4f2db7bcb6728092
Vinayak S <vinayaks439@gmail.com>
2021-09-21 22:29:20 +0530

    Update iap_brand_client.tf

--- iap_brand_client.tf
+++ iap_brand_client.tf
@@ -1,15 +1,15 @@
-resource "google_project_service" "project_service" {
-  project = data.google_project.self.project_id
-  service = "iap.googleapis.com"
-}
+# resource "google_project_service" "project_service" {
+#   project = data.google_project.self.project_id
+#   service = "iap.googleapis.com"
+# }
 
-resource "google_iap_brand" "project_brand" {
-  support_email     = "terraform@vc-fallen-devint.iam.gserviceaccount.com"
-  application_title = "Cloud IAP protected Application"
-}
+# resource "google_iap_brand" "project_brand" {
+#   support_email     = "terraform@vc-fallen-devint.iam.gserviceaccount.com"
+#   application_title = "Cloud IAP protected Application"
+# }
 
-resource "google_iap_client" "project_client" {
-  display_name = "IAPtestAPP"
-  brand        =  google_iap_brand.project_brand.name
-  depends_on = [google_iap_brand.project_brand]
-}
+# resource "google_iap_client" "project_client" {
+#   display_name = "IAPtestAPP"
+#   brand        =  google_iap_brand.project_brand.name
+#   depends_on = [google_iap_brand.project_brand]
+# }
