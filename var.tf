133ebf89e45d3c9c7f46838b62a49a4bf83ae5f8
Vinayak S <vinayaks439@gmail.com>
2021-10-19 13:35:52 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -2,6 +2,11 @@ variable "project_id" {
   default = null
 }
 
+variable "addrname" {
+  type        = list
+  default     = ["addr1","addr2"]
+}
+
 variable "tfe_organisation_name" {
   default = null
 }
